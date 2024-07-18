323137f1ee6b95045de95c3bd862caf404a63162
AliceReturns <Shanisaglass@gmail.com>
2024-07-18 16:17:40 +0200

    Update

---
+++ typescript-exercises/src/01_basic_ts_1_2/basic.ts
@@ -0,0 +1,11 @@
+console.log("Welcome :)");
+
+//let dogName = "Leon";
+//console.log("Leon");
+
+//let dogName: number = `elf`;
+
+let seventeen: number = 17;
+
+const sum = seventeen + 44;
+console.log(sum);
---
+++ typescript-exercises/src/Intro-TS-Level-1_13/basic.ts
@@ -0,0 +1,27 @@
+let x: number = 20;
+let y: number = 30;
+let z: number = 10;
+
+let sum: number = x + y;
+
+console.log("Das Ergebnis der Addition von x und y ist: " + sum);
+
+let subtraktion: number = x - y;
+let subtraktion2: number = y - x;
+
+console.log("Das Ergebnis der Addition von x und y ist: " + sum);
+console.log("Das Ergebnis der Subtraktion von y von x ist: " + subtraktion);
+console.log("Das Ergebnis der Subtraktion von x von y ist: " + subtraktion2);
+
+let multiplication: number = x * y;
+
+console.log(
+  "Das Ergebnis der Multiplikation von x und y ist: " + multiplication
+);
+
+let division: number = x / y;
+
+console.log("Das Ergebnis der Division von x durch y ist: " + division);
+
+let multiplication2: number = x * y;
+let division2: number = multiplication / z;
