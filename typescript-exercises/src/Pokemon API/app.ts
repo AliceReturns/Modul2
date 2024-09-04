63856c87a28ae7f4a5e4c68b17a7f6f25781335c
AliceReturns <Shanisaglass@gmail.com>
2024-09-04 18:02:54 +0200

    Pokemon API

--- typescript-exercises/src/Pokemon API/app.ts
+++ typescript-exercises/src/Pokemon API/app.ts
@@ -1,26 +1,55 @@
 console.clear();
 
-const url = "https://pokeapi.co/api/v2/pokemon/mew";
-
 const form = document.getElementById("searchForm") as HTMLFormElement;
 const loading = document.getElementById("loading") as HTMLDivElement;
 const search = document.getElementById("pokemonNameSearch") as HTMLInputElement;
 const result = document.getElementById("output") as HTMLDivElement;
 
-// async function searchData(params: type) {
-//   fetch(url)
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  loading.classList.remove("d-none");
+  loadPokemon();
+});
 
-//   const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+const getPokemon = async (url: string, text: string) => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw "Please enter a valid Pokemon name or ID";
+    } else if (!text || Number(text) <= 0 || Number(text) >= 650) {
+      throw "Please enter a valid Pokemon name or ID";
+    }
+    const data = await res.json();
+    console.log(data);
+    const {
+      id,
+      name,
+      sprites: {
+        other: {
+          dream_world: { front_default },
+        },
+      },
+    } = data;
 
-//     .then((response) => {
+    loading.classList.add("d-none");
+    result.className = "result active";
+    result.innerHTML = `<div class="pokebox found">
+            <span class="closebox">x</span>
+            <img src="${front_default}" alt="${name}" class="pokemon">
+            <h3 class="pokename">${name}</h3>
+            <p class="pokenumber">#${id.toString().padStart(3, "0")}</p>
+      </div>`;
+    search.value = "";
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.classList.add("d-none");
+  }
+};
 
-//       if (!response.ok) {
-//         throw new Error("Could not fetch resource");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => console.log(data))
-//     .catch((error) =>
-//       console.error("Es gab ein Problem mit dem Abrufen der Daten", error)
-//     );
-// }
+function loadPokemon() {
+  let text = search.value.trim();
+  if (isNaN(Number(text))) text = text.toLowerCase();
+  const url = `https://pokeapi.co/api/v2/pokemon/${text}`;
+  getPokemon(url, text);
+}
